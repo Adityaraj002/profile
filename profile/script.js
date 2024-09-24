7476e841f2aa27f61eb758217777a5aa1efeca9b
Aditya raj <134889731+Adityaraj002@users.noreply.github.com>
2024-09-25 02:50:08 +0530

    Add files via upload

---
+++ profile/script.js
@@ -0,0 +1,21 @@
+const Opengit = document.getElementById("gitOpen")
+
+Opengit.addEventListener("click", () => {
+  window.open("https://github.com/Adityaraj002");
+})
+const OpenTwitter = document.getElementById("TwitterOpen")
+
+OpenTwitter.addEventListener("click", () => {
+  window.open("https://x.com/Aditya_raj9910");
+})
+const OpenInstagram = document.getElementById("InstagramOpen")
+
+OpenInstagram.addEventListener("click", () => {
+  window.open("https://www.instagram.com/desir_aditya.raj/");
+})
+
+const OpenEmail = document.getElementById("EmailOpen")
+
+OpenEmail.addEventListener("click", () => {
+  window.open("https://mail.google.com/mail/u/0/#inbox");
+})
